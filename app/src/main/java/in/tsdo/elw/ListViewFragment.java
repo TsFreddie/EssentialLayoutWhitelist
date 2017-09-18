8378e3402b26034e5703dc64b9cf095c7dddb1f2
Freddie Wang <tsfreddiewang@gmail.com>
2017-09-18 01:38:39 -0700

    clean up

--- app/src/main/java/in/tsdo/elw/ListViewFragment.java
+++ app/src/main/java/in/tsdo/elw/ListViewFragment.java
@@ -3,7 +3,6 @@ package in.tsdo.elw;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
